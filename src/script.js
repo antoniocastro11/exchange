4fdb9389833e0ef22e111915d09beb8bee908d34
castro <antoniog_castro@hotmail.com>
2025-06-07 12:43:45 -0300

    feat: adição da função de conversão bi direcional

--- src/script.js
+++ src/script.js
@@ -1,15 +1,23 @@
-function converter() {
-        const taxa = 5.0;
-        const valorReal = parseFloat(document.getElementById("valor").value);
+    const taxa = 5.00;
 
-        if (isNaN(valorReal) || valorReal <= 0) {
-          document.getElementById("resultado").textContent =
-            "Digite um valor válido.";
-          return;
-        }
+    function converterParaDolar() {
+      const valorReal = parseFloat(document.getElementById('real').value);
 
+      if (!isNaN(valorReal)) {
         const valorDolar = valorReal / taxa;
-        document.getElementById(
-          "resultado"
-        ).textContent = `US$ ${valorDolar.toFixed(2)}`;
+        document.getElementById('dolar').value = valorDolar.toFixed(2);
+      } else {
+        document.getElementById('dolar').value = "";
       }
+    }
+
+    function converterParaReal() {
+      const valorDolar = parseFloat(document.getElementById('dolar').value);
+
+      if (!isNaN(valorDolar)) {
+        const valorReal = valorDolar * taxa;
+        document.getElementById('real').value = valorReal.toFixed(2);
+      } else {
+        document.getElementById('real').value = "";
+      }
+    }
